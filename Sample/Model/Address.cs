dbaeaf7814adeb48ebcf1266f334f3f2c4787a28
SimonCropp <simon.cropp@gmail.com>
2020-04-26 12:06:14 +1000

    .

--- Sample/Model/Address.cs
+++ Sample/Model/Address.cs
@@ -4,7 +4,10 @@
     public string City { get; }
     public string State { get; }
 
-    public Address(string street, string city,string state)
+    public Address(
+        string street,
+        string city,
+        string state)
     {
         Street = street;
         City = city;
--- Sample/Model/Person.cs
+++ Sample/Model/Person.cs
@@ -8,7 +8,12 @@ class Person
     public DateTime DateOfBirth { get; }
     public Address Address { get; }
 
-    public Person(Guid id, string givenNames, string familyName,DateTime dateOfBirth,Address address)
+    public Person(
+        Guid id,
+        string givenNames,
+        string familyName,
+        DateTime dateOfBirth,
+        Address address)
     {
         Id = id;
         GivenNames = givenNames;
