84226051ad2d12b1222807e64f5aea8426c7732c
Simon Cropp <simon.cropp@gmail.com>
2021-03-25 20:00:56 +1100

    .

---
+++ Sample/Blazor/BunitTest.cs
@@ -0,0 +1,21 @@
+﻿using BlazorWebApp.Pages;
+using Bunit;
+using Xunit;
+
+public class BunitTest
+{
+    [Fact]
+    public void RenderCounter()
+    {
+        // Arrange: render the Counter.razor component
+        using var context = new TestContext();
+        var component = context.RenderComponent<Counter>();
+
+        // Act: find and click the <button> element to increment
+        // the counter in the <p> element
+        component.Find("button").Click();
+
+        // Assert: first find the <p> element, then verify its content
+        component.Find("p").MarkupMatches("<p>Current count: 1</p>");
+    }
+}
