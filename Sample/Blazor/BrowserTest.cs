3726f1a48643bfba9d642c495e447dd45c1c4719
Simon Cropp <simon.cropp@gmail.com>
2021-03-25 21:42:47 +1100

    .

--- Sample/Blazor/BrowserTest.cs
+++ Sample/Blazor/BrowserTest.cs
@@ -15,10 +15,19 @@ public class BrowserTest :
     }
 
     [Fact]
-    public async Task RenderIndex()
+    public async Task Index()
     {
         await page.GoToAsync("http://localhost:5000/");
         await page.WaitForLoadStateAsync(LifecycleEvent.Networkidle);
         await Verifier.Verify(page);
+    }
+
+    [Fact]
+    public async Task Counter()
+    {
+        await page.GoToAsync("http://localhost:5000/counter");
+        await page.WaitForLoadStateAsync(LifecycleEvent.Networkidle);
+        var element = await page.QuerySelectorAsync(".content");
+        await Verifier.Verify(element);
     }
 }
