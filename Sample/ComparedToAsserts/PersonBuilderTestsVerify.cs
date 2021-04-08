51820a90c280a40d08a1c5ec4399e5f3a39409bc
Simon Cropp <simon.cropp@gmail.com>
2021-04-08 19:23:31 +1000

    Update PersonBuilderTestsVerify.cs

--- Sample/ComparedToAsserts/PersonBuilderTestsVerify.cs
+++ Sample/ComparedToAsserts/PersonBuilderTestsVerify.cs
@@ -1,31 +1,36 @@
 ﻿using System;
 using System.Linq;
 using System.Threading.Tasks;
+using VerifyTests;
 using VerifyXunit;
 using Xunit;
 
 [UsesVerify]
 public class PersonBuilderTestsVerify
 {
+    static VerifySettings verifySettings;
+
+    static PersonBuilderTestsVerify()
+    {
+        verifySettings = new VerifySettings();
+        verifySettings.ScrubInlineGuids();
+        verifySettings.ModifySerialization(
+            settings => settings.IgnoreMember<Person>(target => target.Age));
+    }
+
     [Fact]
     public async Task Find()
     {
         var id = new Guid("ebced679-45d3-4653-8791-3d969c4a986c");
         var person = PersonBuilder.Find(id)!;
         Assert.Equal((DateTime.Now - person.DateOfBirth).TotalDays, person.Age.TotalDays, 1);
-        await Verifier.Verify(person)
-            .ScrubInlineGuids()
-            .ModifySerialization(
-                settings => settings.IgnoreMember<Person>(target => target.Age));
+        await Verifier.Verify(person, verifySettings);
     }
 
     [Fact]
     public async Task FindAll()
     {
         var people = PersonBuilder.FindAll().ToList();
-        await Verifier.Verify(people)
-            .ScrubInlineGuids()
-            .ModifySerialization(
-                settings => settings.IgnoreMember<Person>(target => target.Age));
+        await Verifier.Verify(people, verifySettings);
     }
 }
