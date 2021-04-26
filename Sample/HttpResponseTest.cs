432e1ef045392dfe962e2b2771153ec8edc7bd53
Simon Cropp <simon.cropp@gmail.com>
2021-04-26 17:01:43 +1000

    .

---
+++ Sample/HttpResponseTest.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using VerifyTests;
+using VerifyXunit;
+using Xunit;
+
+[UsesVerify]
+public class HttpResponseTest
+{
+    [ModuleInitializer]
+    public static void Init()
+    {
+        VerifierSettings.ScrubLinesContaining(
+            "Traceparent",
+            "X-Amzn-Trace-Id",
+            "origin",
+            "Content-Length",
+            "TrailingHeaders");
+    }
+
+    [Fact]
+    public async Task ImageHttpResponse()
+    {
+        using HttpClient client = new();
+
+        var result = await client.GetAsync("https://httpbin.org/image/png");
+
+        await Verifier.Verify(result);
+    }
+
+    [Fact]
+    public async Task HttpResponse()
+    {
+        using HttpClient client = new();
+
+        var result = await client.GetAsync("https://httpbin.org/get");
+
+        await Verifier.Verify(result);
+    }
+}
