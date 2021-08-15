fcc4ed604fd4e2a48c87cca864ff5f581dc7f647
Simon Cropp <simon.cropp@gmail.com>
2021-08-16 08:35:40 +1000

    Update ModuleInitializer.cs

--- WinFormsAppTests/ModuleInitializer.cs
+++ WinFormsAppTests/ModuleInitializer.cs
@@ -6,7 +6,7 @@ public static class ModuleInitializer
     [ModuleInitializer]
     public static void Initialize()
     {
-        VerifyImageMagick.RegisterComparers(.01);
+        VerifyImageMagick.RegisterComparers(.014);
         VerifyWinForms.Enable();
     }
 }
