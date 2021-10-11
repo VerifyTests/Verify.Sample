850cd01adcb4d92c3cc53807916204e1e8c39bed
Simon Cropp <simon.cropp@gmail.com>
2021-10-11 20:48:05 +1100

    VerifyHttp.Enable

--- Sample/ModuleInitializer.cs
+++ Sample/ModuleInitializer.cs
@@ -6,6 +6,7 @@ public static class ModuleInitializer
     [ModuleInitializer]
     public static void Initialize()
     {
+        VerifyHttp.Enable();
         VerifyImageMagick.RegisterComparers(.01);
 
         VerifierSettings.ModifySerialization(settings =>
