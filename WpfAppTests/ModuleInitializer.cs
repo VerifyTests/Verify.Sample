5983c79c9600bb930197b588669c99fc18388a1e
Simon Cropp <simon.cropp@gmail.com>
2022-01-06 05:43:39 +1100

    Update ModuleInitializer.cs

--- WpfAppTests/ModuleInitializer.cs
+++ WpfAppTests/ModuleInitializer.cs
@@ -5,7 +5,7 @@ public static class ModuleInitializer
     [ModuleInitializer]
     public static void Initialize()
     {
-        VerifyImageMagick.RegisterComparers(.019);
+        VerifyImageMagick.RegisterComparers(.025);
         VerifyXaml.Enable();
     }
 }
