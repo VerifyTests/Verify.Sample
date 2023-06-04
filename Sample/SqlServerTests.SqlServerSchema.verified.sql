3e6a48f5c9ee2450a7b16bb60460c70598cbc624
Simon Cropp <simon.cropp@gmail.com>
2023-06-04 20:52:14 +1000

    Update SqlServerTests.SqlServerSchema.verified.sql

--- Sample/SqlServerTests.SqlServerSchema.verified.sql
+++ Sample/SqlServerTests.SqlServerSchema.verified.sql
@@ -1,23 +1,33 @@
--- Tables
+﻿-- Tables
+
 CREATE TABLE [dbo].[MyTable](
 	[Value] [int] NULL
 ) ON [PRIMARY]
+
 CREATE NONCLUSTERED INDEX [MyIndex] ON [dbo].[MyTable]
 (
 	[Value] ASC
 ) ON [PRIMARY]
+
 CREATE TRIGGER MyTrigger
 ON MyTable
 AFTER UPDATE
 AS RAISERROR ('Notify Customer Relations', 16, 10);
+
 ALTER TABLE [dbo].[MyTable] ENABLE TRIGGER [MyTrigger]
+
+
 -- Views
+
 CREATE VIEW MyView
 AS
   SELECT Value
   FROM MyTable
   WHERE (Value > 10);
+
+
 -- StoredProcedures
+
 CREATE PROCEDURE MyProcedure
 AS
 BEGIN
@@ -26,7 +36,10 @@ BEGIN
   FROM MyTable
   WHERE (Value > 10);
 END;
+
+
 -- UserDefinedFunctions
+
 CREATE FUNCTION MyFunction(
   @quantity INT,
   @list_price DEC(10,2),
@@ -37,5 +50,8 @@ AS
 BEGIN
     RETURN @quantity * @list_price * (1 - @discount);
 END;
+
+
 -- Synonyms
+
 CREATE SYNONYM [dbo].[synonym1] FOR [MyTable]
